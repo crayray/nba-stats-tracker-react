ec6b801b094bd8089ba2afe96c8515e67fe9e237
github username <github email address>
2020-01-17 11:45:29 -0600

    Working on routes, in progress

--- src/index.js
+++ src/index.js
@@ -2,9 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Navbar from './Navbar';
+import Signup from './Signup';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render((  <Router>
+    <div>
+      <Navbar />
+      <Route exact path="/" component={App} />
+      <Route exact path="/signup" component={Signup} />
+    </div>
+  </Router>), document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
