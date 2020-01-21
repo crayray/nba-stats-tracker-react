4b97f23741120dea495ecbd9a06883802a09cb2f
github username <github email address>
2020-01-21 11:12:33 -0600

    Passed props to Team Card

---
+++ src/Team.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Teams from "./data/all_teams.json";
+import TeamCard from "./TeamCard";
+
+export default function Team() {
+  return (
+    <div>
+      {Teams.api.teams
+        .filter(team => team.nbaFranchise === "1")
+        .map(team => (
+          <TeamCard key={team.teamId} team={team}/>
+        ))}
+    </div>
+  );
+}
---
+++ src/TeamCard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function TeamCard({team}) {
+    return (
+        <div>
+            <h4>{team.fullName}</h4>
+        </div>
+    )
+}
