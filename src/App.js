f4baaa3f6450cdb660ce6e773e79bf9bc54448bf
rachfuerst <rach.fuerst@gmail.com>
2020-01-22 10:14:40 -0600

    Widening the data window container

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ class App extends React.Component {
 
   render() {
     return (
-      <Container style={{ marginTop: "5em"}}>
+      <Container style={{ marginTop: "5em" }}>
         <Segment raised style={{ overflow: "auto", maxHeight: "28em" }}>
           <DataMenu />
           <Grid>
