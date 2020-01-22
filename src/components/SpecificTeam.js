c5dd1d29f9cec59c7599ce59c813a73b7b947379
rachfuerst <rach.fuerst@gmail.com>
2020-01-22 09:15:47 -0600

    Styling the specific team cards by adding the Card component

--- src/components/SpecificTeam.js
+++ src/components/SpecificTeam.js
@@ -1,16 +1,18 @@
-import React from 'react'
-
+import React from "react";
+import { Card, Image } from "semantic-ui-react";
 
 export default class SpecificTeam extends React.Component {
-    render() {
+  render() {
     return (
-        <div>
-            <img src={this.props.team.logo} alt="Couldn't Load"></img>
-            <h1>{this.props.team.fullName}</h1>
-            <h2>City: {this.props.team.city}</h2>
-            <h3>Conference: {this.props.team.leagues.standard.confName}</h3>
-            <h3>Division: {this.props.team.leagues.standard.divName}</h3>
-
-        </div>
-    )}
+      <Card>
+        <Card.Content>
+          <Image floated="left" size="small" src={this.props.team.logo} />
+          <h1>{this.props.team.fullName}</h1>
+          <h2>City: {this.props.team.city}</h2>
+          <h3>Conference: {this.props.team.leagues.standard.confName}</h3>
+          <h3>Division: {this.props.team.leagues.standard.divName}</h3>
+        </Card.Content>
+      </Card>
+    );
+  }
 }
