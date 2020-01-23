edc7032b6a4678979fcba7348c0318da3d2edd2e
github username <github email address>
2020-01-23 17:08:47 -0600

    Fixed sign in bug with sign up

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import { NavLink } from 'react-router-dom';
 import { Menu, Container, Image } from "semantic-ui-react";
-function NavBar() {
+function NavBar(props) {
   return (
     <div className="App">
         <NavLink to="/">
@@ -15,7 +15,7 @@ function NavBar() {
         <Menu.Item as='a'>Home</Menu.Item>
         <Menu.Item as='a'><NavLink to='/signup'>Login</NavLink></Menu.Item>
         <Menu.Item as='a'><NavLink to='/signup'>Sign Up</NavLink></Menu.Item>
-
+        
       </Container>
     </Menu>
       </NavLink>
